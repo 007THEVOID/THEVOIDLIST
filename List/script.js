f5fcf265f283c781e5a3fb80bc03aef63714fe17
007THEVOID <arya.rio.aria@gmail.com>
2026-01-31 18:08:03 +0100

    Update script.js

--- List/script.js
+++ List/script.js
@@ -18,6 +18,22 @@ const TOKENS = {
 USDT: {
 address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
 decimals: 6
+},
+USDC: {
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606EB48",
+    decimals: 6
+},
+  DAI: {
+    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    decimals: 18
+},
+  WBTC: {
+    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    decimals: 8
+},
+  BNB: {
+    address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52",
+    decimals: 18
 }
 };
 
