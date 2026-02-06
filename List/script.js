05dbbe715939562e5cd4ffad8860d9b8c1b53b4d
007THEVOID <arya.rio.aria@gmail.com>
2026-02-06 13:20:27 +0100

    Update script.js

--- List/script.js
+++ List/script.js
@@ -103,7 +103,7 @@ async function connectWalletConnect() {
     const accounts = await wcProvider.enable();
     window.ethereum = wcProvider;
 
-    window.web3 = new web3(window.ethereum);
+    window.web3 = new Web3(window.ethereum);
 
     provider = new ethers.providers.Web3Provider(wcProvider);
     signer = provider.getSigner();
